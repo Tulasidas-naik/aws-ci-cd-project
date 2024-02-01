96ef480e9134485d10961c2ac08598b6832bf133
Tulasidas-naik <48889634+Tulasidas-naik@users.noreply.github.com>
2024-02-01 21:58:02 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull dasaf5/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 dasaf5/simple-python-flask-app:latest
